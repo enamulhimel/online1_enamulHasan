fc9a2069201ba59847d6c03020ab4562ce823fc9
Rezab <rdawla191187@bscse.uiu.ac.bd>
2022-07-26 12:28:48 +0600

    query added

--- cancel.php
+++ cancel.php
@@ -4,6 +4,9 @@
     $connect = mysqli_connect("localhost", "root", "", "bus_ticket_management")
         or die("Error connecting Database");
 
+    $query = "DELETE FROM `ticket_booking` WHERE `ticket_booking`.`id` = $id ";
+    mysqli_query($connect, $query)
+    or die("Can not execute query");
     
 
 ?>
