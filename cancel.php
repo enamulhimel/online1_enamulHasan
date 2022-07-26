87d2a18f76e206ec5c3605ad8cd82e5f7a6fd7bc
Rezab <rdawla191187@bscse.uiu.ac.bd>
2022-07-26 12:29:34 +0600

    headed location added after succesful deletion

--- cancel.php
+++ cancel.php
@@ -7,6 +7,8 @@
     $query = "DELETE FROM `ticket_booking` WHERE `ticket_booking`.`id` = $id ";
     mysqli_query($connect, $query)
     or die("Can not execute query");
-    
+
+
+    header('Location: index.php');
 
 ?>
